664cb9fa6c638dfcc01de0585032ebb0bd0a974b
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-02 10:23:06 -0500

    -Agregado metodos genericos de getById(), postData() en la clase RequestCaller

--- app/src/androidTest/java/com/example/okylifeapp/app/rest/RequestCaller.java
+++ app/src/androidTest/java/com/example/okylifeapp/app/rest/RequestCaller.java
@@ -1,6 +1,12 @@
 package com.example.okylifeapp.app.rest;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpParams;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mordreth on 10/2/15.
@@ -9,4 +15,19 @@ public class RequestCaller {
     String hostUrl = "https://okylifeapi.herokuapp.com";
     HttpParams parameters;
 
+    public void getById(String classUriPath, long id, AsyncResponse responseHandler) {
+        String url = this.hostUrl + "/" + classUriPath + "/show";
+        List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+        parameters.add(new BasicNameValuePair("id", String.valueOf(id)));
+        url += "?" + URLEncodedUtils.format(parameters, "ISO-8859-1");
+        GetClient getClient = new GetClient(url, responseHandler);
+        getClient.execute();
+    }
+
+    public void postData(String classUriPath, AsyncResponse responseHandler, List<NameValuePair> params) {
+        String url = this.hostUrl + "/" + classUriPath;
+        PostClient postClient = new PostClient(url, responseHandler, params);
+        postClient.execute();
+
+    }
 }
