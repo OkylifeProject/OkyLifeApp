c312be0e43e0cb0d11e2ab25cf09608779f5352a
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-02 09:47:03 -0500

    -modificada interfaz de respuesta asyncrona

--- app/src/androidTest/java/com/example/okylifeapp/app/rest/AsyncResponse.java
+++ app/src/androidTest/java/com/example/okylifeapp/app/rest/AsyncResponse.java
@@ -3,5 +3,6 @@ package com.example.okylifeapp.app.rest;
 /**
  * Created by mordreth on 10/2/15.
  */
-public class AsyncResponse {
+public interface AsyncResponse {
+    void processFinish(String result);
 }
