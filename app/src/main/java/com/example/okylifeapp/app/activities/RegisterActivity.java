d1c0e31ff6ef81ff379c46b87544f32e42af3e01
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-03 14:47:59 -0500

    -Agregada actividad RegisterWithGoogle

--- app/src/main/java/com/example/okylifeapp/app/activities/RegisterActivity.java
+++ app/src/main/java/com/example/okylifeapp/app/activities/RegisterActivity.java
@@ -8,37 +8,22 @@ import android.view.Window;
 import android.widget.Toast;
 import aplication.OkyLife;
 import com.example.okylifeapp.app.R;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.plus.Plus;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import rest.AsyncResponse;
+
+import java.util.ArrayList;
 
 /**
  * Created by mordreth on 10/2/15.
  */
-public class RegisterActivity extends Activity implements AsyncResponse, GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        View.OnClickListener {
-    /* Request code used to invoke sign in user interactions. */
-    private static final int RC_SIGN_IN = 0;
-    /* Client used to interact with Google APIs. */
-    private GoogleApiClient mGoogleApiClient;
+public class RegisterActivity extends Activity implements AsyncResponse {
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.register);
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Plus.API)
-                .addScope(new Scope(Scopes.PLUS_ME))
-                .addScope(new Scope(Scopes.PROFILE))
-                .build();
 
     }
 
@@ -50,8 +35,9 @@ public class RegisterActivity extends Activity implements AsyncResponse, GoogleA
 
     public void register(View view) {
         /*Prueba para POST*/
-        /*
+
         ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+
         params.add(new BasicNameValuePair("registerType", "Api"));
         params.add(new BasicNameValuePair("sex", "Male"));
         params.add(new BasicNameValuePair("firstName", "Alejo"));
@@ -61,28 +47,9 @@ public class RegisterActivity extends Activity implements AsyncResponse, GoogleA
         params.add(new BasicNameValuePair("birthDate", "5/3/1994"));
 
         ((OkyLife) getApplication()).getMasterCaller().postData("User/registerUser", this, params);
-        */
-        /*Prueba para GET*/
+
+        /*Preba para GET*/
         ((OkyLife) getApplication()).getMasterCaller().getById("User", 1, this);
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-    }
 }
---
+++ app/src/main/java/com/example/okylifeapp/app/activities/RegisterWithGoogleActivity.java
@@ -0,0 +1,9 @@
+package com.example.okylifeapp.app.activities;
+
+/**
+ * Created by mordreth on 10/3/15.
+ */
+public class RegisterWithGoogleActivity {
+
+}
+
