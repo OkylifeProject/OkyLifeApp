2dd6861fbe2ba9a689bb1a41db9e057c35ee910b
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-03 13:59:21 -0500

    -Agregados botonoes de login y register con google plus

--- app/src/main/java/com/example/okylifeapp/app/activities/RegisterActivity.java
+++ app/src/main/java/com/example/okylifeapp/app/activities/RegisterActivity.java
@@ -8,17 +8,38 @@ import android.view.Window;
 import android.widget.Toast;
 import aplication.OkyLife;
 import com.example.okylifeapp.app.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
 import rest.AsyncResponse;
 
 /**
  * Created by mordreth on 10/2/15.
  */
-public class RegisterActivity extends Activity implements AsyncResponse {
+public class RegisterActivity extends Activity implements AsyncResponse, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.register);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PLUS_ME))
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();
+
     }
 
     @Override
@@ -43,5 +64,25 @@ public class RegisterActivity extends Activity implements AsyncResponse {
         */
         /*Prueba para GET*/
         ((OkyLife) getApplication()).getMasterCaller().getById("User", 1, this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
