466a27519fcc56b6df5f879f47b46e11445c89a7
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-04 11:11:24 -0500

    -Creada Actividad de Login con Google

---
+++ app/src/main/java/com/example/okylifeapp/app/activities/LoginWithGoogleActivity.java
@@ -0,0 +1,14 @@
+package com.example.okylifeapp.app.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mordreth on 10/4/15.
+ */
+public class LoginWithGoogleActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInsatnce){
+        super.onCreate(savedInsatnce);
+    }
+}
