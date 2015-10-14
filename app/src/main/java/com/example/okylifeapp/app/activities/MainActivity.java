64c9be36bb530d17634f2896a0a3e3348f167dae
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-14 08:42:14 -0500

    -Agregada actividad para editar perfil -Corregido boton de google en la vista de login

--- app/src/main/java/com/example/okylifeapp/app/activities/MainActivity.java
+++ app/src/main/java/com/example/okylifeapp/app/activities/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.okylifeapp.app.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.example.okylifeapp.app.R;
 
 /**
  * Created by mordreth on 10/4/15.
  */
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_view);
+    }
+
+    public void editProfile() {
+        Intent intent = new Intent(this, ProfileSettingsActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/okylifeapp/app/activities/ProfileSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.okylifeapp.app.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mordreth on 10/14/15.
+ */
+public class ProfileSettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+    }
+}
