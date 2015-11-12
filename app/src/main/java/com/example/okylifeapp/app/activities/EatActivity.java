5eb4f074732a2c1164f27b4cf9697c0d9cd8a8c3
Alejandro Velasco <savelascod@unal.edu.co>
2015-11-12 15:58:21 -0500

    -Aliments saved and listed in EatActivity

--- app/src/main/java/com/example/okylifeapp/app/activities/EatActivity.java
+++ app/src/main/java/com/example/okylifeapp/app/activities/EatActivity.java
@@ -2,9 +2,13 @@ package com.example.okylifeapp.app.activities;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.okylifeapp.app.R;
 import dialogs.LogoutDialog;
@@ -26,6 +30,8 @@ public class EatActivity extends Activity implements AsyncResponse {
     static final int ADD_ALIMENT_REQUEST = 1;
 
     JSONArray jsonAliments;
+    String[] nameAliments;
+    String[] descriptionAliments;
 
     @Override
     public void onCreate(Bundle savedInstance) {
@@ -99,7 +105,7 @@ public class EatActivity extends Activity implements AsyncResponse {
                 try {
                     JSONObject jsonAliment = new JSONObject(data.getStringExtra("jsonAliment"));
                     jsonAliments.put(jsonAliment);
-                    Toast.makeText(getApplicationContext(), jsonAliments.toString(), Toast.LENGTH_LONG).show();
+                    showAliments();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -110,9 +116,54 @@ public class EatActivity extends Activity implements AsyncResponse {
         }
     }
 
+    public void showAliments() {
+        ListView alimentList = (ListView) findViewById(R.id.listAliments);
+        nameAliments = new String[jsonAliments.length()];
+        descriptionAliments = new String[jsonAliments.length()];
+
+        for (int i = 0; i < jsonAliments.length(); i++) {
+            try {
+                JSONObject aliment = (JSONObject) jsonAliments.get(i);
+                nameAliments[i] = aliment.getString("name");
+                descriptionAliments[i] = aliment.getString("description");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        alimentList.setAdapter(new MyAdapter(this, R.layout.row, nameAliments));
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    public class MyAdapter extends ArrayAdapter<String> {
+
+        public MyAdapter(Context context, int textViewResourceId, String[] objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        public View getCustomView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View row = inflater.inflate(R.layout.aliment_row, parent, false);
+            TextView nameText = (TextView) row.findViewById(R.id.aliment_name);
+            TextView descriptionText = (TextView) row.findViewById(R.id.aliment_description);
+            nameText.setText(nameAliments[position]);
+            descriptionText.setText(descriptionAliments[position]);
+            return row;
+        }
     }
 }
