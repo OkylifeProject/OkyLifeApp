443acdf250fa4049ca25819be6b0a27d837ea9fe
Alejandro Velasco <savelascod@unal.edu.co>
2015-11-15 17:50:51 -0500

    -Added class Location

---
+++ app/src/main/java/data/Location.java
@@ -0,0 +1,30 @@
+package data;
+
+/**
+ * Created by mordreth on 11/15/15.
+ */
+public class Location {
+    private double longitude;
+    private double latitude;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
