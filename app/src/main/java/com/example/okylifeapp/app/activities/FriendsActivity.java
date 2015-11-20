cde9f93081892a0bc0ab1c03555655b356dc3329
Alejandro Velasco <savelascod@unal.edu.co>
2015-11-20 18:31:24 -0500

    -Friends Finer completely working

--- app/src/main/java/com/example/okylifeapp/app/activities/FriendsActivity.java
+++ app/src/main/java/com/example/okylifeapp/app/activities/FriendsActivity.java
@@ -3,7 +3,9 @@ package com.example.okylifeapp.app.activities;
 import android.accounts.Account;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import aplication.OkyLife;
 import com.example.okylifeapp.app.R;
@@ -22,10 +24,11 @@ import java.util.ArrayList;
  * Created by mordreth on 11/17/15.
  */
 public class FriendsActivity extends Activity implements AsyncResponse {
-    JSONArray jsonFriends;
+    JSONArray jsonUsers;
     ArrayList<User> users;
     private Account okyLifeAccount;
     private ListView friendList;
+    private MyFriendAdapter serviceAdapter;
 
     @Override
     public void onCreate(Bundle savedInstance) {
@@ -33,6 +36,8 @@ public class FriendsActivity extends Activity implements AsyncResponse {
         setContentView(R.layout.friends_view);
         users = new ArrayList<User>();
         okyLifeAccount = ((OkyLife) getApplication()).getOkyLifeAccount();
+        friendList = (ListView) findViewById(R.id.friend_list);
+        //setListUserAdapter();
         getFriends();
     }
 
@@ -45,48 +50,69 @@ public class FriendsActivity extends Activity implements AsyncResponse {
 
     @Override
     public void processFinish(String result) {
-        setFriends(result);
-        setListAdapter();
+        setUsers(result);
+        setListUserAdapter();
     }
 
-    private void setListAdapter() {
+    private void updateListAdapter() {
+        ((BaseAdapter) friendList.getAdapter()).notifyDataSetChanged();
+    }
+
+    private void setListUserAdapter() {
         // instantiate our ItemAdapter class
-        friendList = (ListView) findViewById(R.id.friend_list);
-        MyFriendAdapter serviceAdapter = new MyFriendAdapter(this, R.layout.friend_row, users);
+        serviceAdapter = new MyFriendAdapter(this, R.layout.friend_row, users);
         friendList.setAdapter(serviceAdapter);
     }
 
-    private void setFriends(String result) {
 
+    private void setUsers(String result) {
+        users = new ArrayList<User>();
         try {
-            jsonFriends = new JSONArray(result);
+            jsonUsers = new JSONArray(result);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         String imageBytes;
-        if (jsonFriends != null) {
-            for (int i = 0; i < jsonFriends.length(); i++) {
-                JSONObject friend = null;
+        if (jsonUsers != null) {
+            for (int i = 0; i < jsonUsers.length(); i++) {
+                JSONObject user = null;
                 try {
-                    friend = (JSONObject) jsonFriends.get(i);
+                    user = (JSONObject) jsonUsers.get(i);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
                 try {
-                    imageBytes = friend.getString("imageBytes");
+                    imageBytes = user.getString("imageBytes");
                 } catch (JSONException e) {
                     imageBytes = null;
                 }
                 try {
-                    users.add(new User(friend.getString("name"),
+                    users.add(new User(user.getString("name"),
                             imageBytes,
-                            friend.getString("email"),
-                            friend.getString("id"), true));
+                            user.getString("email"),
+                            user.getString("id"),
+                            user.getBoolean("isFriend")));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-            Log.v("friends", String.valueOf(users.size()));
         }
+    }
+
+    public void getUsersByName(String userName) {
+        ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("email", okyLifeAccount.name));
+        params.add(new BasicNameValuePair("userName", userName));
+        ((OkyLife) getApplication()).getMasterCaller().postData("User/getUsersByName", this, params);
+    }
+
+    public void searchUsers(View view) {
+        EditText entryText = (EditText) findViewById(R.id.search_friend_box);
+        if (entryText.getText().toString().equals("")) {
+            getFriends();
+        } else {
+            getUsersByName(entryText.getText().toString());
+        }
+
     }
 }
