dea5d7838629cdb52fe797d716d1223907d3bdbf
Alejandro Velasco <savelascod@unal.edu.co>
2015-11-20 20:26:20 -0500

    -Added Inbox Activity -Added Inbox main View -Added message row

---
+++ app/src/main/java/com/example/okylifeapp/app/activities/InboxActivity.java
@@ -0,0 +1,15 @@
+package com.example.okylifeapp.app.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mordreth on 11/20/15.
+ */
+public class InboxActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+    }
+}
